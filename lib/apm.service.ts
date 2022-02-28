b059502c7846894e42af4e53e0f1a3e7afcf0075
Miha Primc <miha.primc@beenius.tv>
2022-02-28 12:25:15 +0100

    New: Support new APM agent API

--- lib/apm.service.ts
+++ lib/apm.service.ts
@@ -1,35 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import * as APM from 'elastic-apm-node';
+import apm = require('elastic-apm-node');
 
 @Injectable()
 export class ApmService {
-  apm: any;
+  private readonly apm: apm.Agent;
 
   constructor() {
     this.apm = APM;
   }
 
-  captureError(data: any) {
+  captureError(data: any): void {
     this.apm.captureError(data);
   }
 
-  startTransaction(name, type): any {
-    return this.apm.startTransaction(name, type);
+  startTransaction(name?: string, options?: apm.TransactionOptions): apm.Transaction {
+    return this.apm.startTransaction(name, options);
   }
 
-  setTransactionName(name) {
-    return this.apm.setTransactionName(name);
+  setTransactionName(name: string): void {
+    this.apm.setTransactionName(name);
   }
 
-  startSpan(name) {
-    return this.apm.startSpan(name);
+  startSpan(name?: string, options?: apm.SpanOptions): apm.Span {
+    return this.apm.startSpan(name, options);
   }
 
-  setCustomContext(context) {
-    return this.apm.setCustomContext(context);
-  }
-
-  setTag(name, value) {
-    return this.apm.setTag(name, value);
+  setCustomContext(context: Record<string, unknown>): void {
+    this.apm.setCustomContext(context);
   }
 }
